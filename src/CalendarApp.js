4c1e4f488ee1f18e963d88a9859dce396afbe90c
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-08-18 13:03:34 -0500

    Add ToastContainer to show notifications

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,6 +1,25 @@
 import React from 'react';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import AppRouter from 'router/AppRouter';
 
 export default function CalendarApp() {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <ToastContainer
+        position='top-right'
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover
+      />
+    </>
+  );
 }
