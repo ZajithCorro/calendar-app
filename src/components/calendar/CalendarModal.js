63b9ee73999e22edcfbea4e0fb4dd5a9db476e19
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-08-18 13:05:00 -0500

    CalendarModal component to create a new event

---
+++ src/components/calendar/CalendarModal.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+
+import moment from 'moment';
+import Modal from 'react-modal';
+import DateTimePicker from 'react-datetime-picker';
+import { toast } from 'react-toastify';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
+
+const now = moment().minutes(0).seconds(0).add(1, 'hours');
+const end = now.clone().add(1, 'hours');
+
+export default function CalendarModal() {
+  const [startDate, setStartDate] = useState(now.toDate());
+  const [endDate, setEndDate] = useState(end.toDate());
+  const [isInvalidTitle, setIsInvalidTitle] = useState(false);
+
+  const [formValues, setFormValues] = useState({
+    title: 'Evento',
+    notes: '',
+    start: now.toDate(),
+    end: end.toDate(),
+  });
+
+  const { title, notes } = formValues;
+
+  const handleInputChange = ({ target }) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
+
+  const closeModal = () => {};
+
+  const handleStartDateChange = (e) => {
+    setStartDate(e);
+    setFormValues({
+      ...formValues,
+      start: e,
+    });
+  };
+
+  const handleEndDateChange = (e) => {
+    setEndDate(e);
+    setFormValues({
+      ...formValues,
+      end: e,
+    });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    const { title, start, end } = formValues;
+    const momentStart = moment(start);
+    const momentEnd = moment(end);
+
+    if (momentStart.isSameOrAfter(momentEnd)) {
+      toast.error(' ⚠ La fecha final debe ser mayor a la fecha de inicio');
+    }
+
+    if (title.trim().length < 2) {
+      setIsInvalidTitle(true);
+      return;
+    }
+
+    closeModal();
+    setIsInvalidTitle(false);
+  };
+
+  return (
+    <Modal
+      isOpen={true}
+      style={customStyles}
+      onRequestClose={closeModal}
+      closeTimeoutMS={200}
+      contentLabel='Example Modal'
+      className='modal'
+      overlayClassName='modal-fondo'
+    >
+      <h1> Nuevo evento </h1>
+      <hr />
+      <form className='container' onSubmit={handleSubmitForm}>
+        <div className='form-group'>
+          <label>Fecha y hora inicio</label>
+          <DateTimePicker
+            onChange={handleStartDateChange}
+            value={startDate}
+            className='form-control'
+          />
+        </div>
+
+        <div className='form-group'>
+          <label>Fecha y hora fin</label>
+          <DateTimePicker
+            onChange={handleEndDateChange}
+            value={endDate}
+            minDate={startDate}
+            className='form-control'
+          />
+        </div>
+
+        <hr />
+        <div className='form-group'>
+          <label>Titulo y notas</label>
+          <input
+            type='text'
+            className={`form-control ${isInvalidTitle && 'is-invalid'}`}
+            placeholder='Título del evento'
+            name='title'
+            autoComplete='off'
+            value={title}
+            onChange={handleInputChange}
+          />
+          <small id='emailHelp' className='form-text text-muted'>
+            Una descripción corta
+          </small>
+        </div>
+
+        <div className='form-group'>
+          <textarea
+            type='text'
+            className='form-control'
+            placeholder='Notas'
+            rows='5'
+            name='notes'
+            value={notes}
+            onChange={handleInputChange}
+          ></textarea>
+          <small id='emailHelp' className='form-text text-muted'>
+            Información adicional
+          </small>
+        </div>
+
+        <button type='submit' className='btn btn-outline-primary btn-block'>
+          <i className='far fa-save'></i>
+          <span> Guardar</span>
+        </button>
+      </form>
+    </Modal>
+  );
+}
