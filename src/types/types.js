65b234cc356564f2043d814b14ea297a6fb70a17
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-08-27 14:27:47 -0500

    Auth reducer and types

--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,12 @@ export const types = {
   EVENT_CLEAR_ACTIVE_EVENT: '[EVENT] Clear active event',
   EVENT_UPDATE: '[EVENT] Event updated',
   EVENT_DELETE: '[EVENT] Event deleted',
+
+  AUTH_CHECKING: '[AUTH] Checkin login state',
+  AUTH_CHECKING_FINISH: '[AUTH] Finish checking login state',
+  AUTH_START_LOGIN: '[AUTH] Start login',
+  AUTH_LOGIN: '[AUTH] Login',
+  AUTH_START_REGISTER: '[AUTH] Start register',
+  AUTH_START_TOKEN_RENEW: '[AUTH] Start token renew',
+  AUTH_LOGOUT: '[AUTH] Logout',
 };
